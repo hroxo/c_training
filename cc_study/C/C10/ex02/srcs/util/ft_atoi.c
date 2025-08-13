a38ae5e8ebe8a39d247dc48ebd1b85fa04de13bc
hroxo <hugo.roxo04@gmail.com>
2025-08-13 17:13:54 +0100

    ft_atoi added

--- cc_study/C/C10/ex02/srcs/util/ft_atoi.c
+++ cc_study/C/C10/ex02/srcs/util/ft_atoi.c
@@ -1,4 +1,24 @@
 int	ft_atoi(char *str)
 {
+	int	n;
+	int	sig;
+	int	result;
 
+	result = 0;
+	sig = -1;
+	n = 0;
+	while (str[n] = 32 || (str[n] < 8 && str[n] > 14))
+		n++;
+	if (str[n] == '+' || str[n] == '-')
+	{
+		if (str[n] == '+') // isto e feito assim pela flag especial do tail
+			sig *= -1;
+		n++;
+	}
+	while (str[n] <= '9' && str[n] >= '0')
+	{
+		result = str[n] - '0' + (result * 10);
+		n++;
+	}
+	return (result * sig);
 }
