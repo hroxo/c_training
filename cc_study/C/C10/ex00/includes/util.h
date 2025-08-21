a3648b778a131b9434927a41efc0819db3eee175
Hugo <hugo.roxo04@gmail.com>
2025-08-21 20:40:14 +0200

    redirected content to correct repositorie

--- cc_study/C/C10/ex00/includes/util.h
+++
@@ -1,9 +0,0 @@
-#ifndef UTIL_H
-#define UTIL_H
-
-#define FEW_ARG "File name missing.\n"
-#define MANY_ARG "Too many arguments.\n"
-#define NO_READBLE_ARG "Cannot read file.\n"
-
-#endif
-
--- cc_study/C/C11/ex05/srcs/util/ft_strcmp.c
+++
@@ -1,9 +0,0 @@
-int	ft_strcmp(char *a, char *b)
-{
-	int	i;
-
-	i = 0;
-	while (a[i] && a[i] == b[i])
-		i++;
-	return (a[i] - b[i]);
-}
